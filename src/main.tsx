9697cdd36a74597ea66978a442a3b6196ad0a246
Sindhura Mandepudi <sindhura.ull@gmail.com>
2025-09-30 13:53:09 -0700

    using Matreial UI and removing Tailwind css

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,21 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 
+const theme = createTheme({
+  palette: {
+    primary: { main: "#1976d2" }, // blue
+    secondary: { main: "#dc004e" }, // red/pink
+  },
+});
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HashRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </HashRouter>
   </StrictMode>
 );
