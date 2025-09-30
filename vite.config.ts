41873cc177179fca1e1f1b0cdefdc0ee165d691b
Sindhura Mandepudi <sindhura.ull@gmail.com>
2025-09-30 10:56:18 -0700

    updating vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/React-Counter/',   // <-- Add this linenpm run build
-  server: {
-    open: true,  // 👈 this makes it auto-open in browser
-  },
+  base: '/React-Counter/'   // <-- Add this linenpm run build
 })
