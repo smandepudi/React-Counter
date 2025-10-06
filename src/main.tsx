7d0e37bbe73de00bc7ec1994bbc18ea9cb145844
Sindhura Mandepudi <sindhura.ull@gmail.com>
2025-10-06 12:38:22 -0700

    adding redux persist to save todos to local storage so they remain intact on page reloads

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import "./index.css";
-import { store } from "./store";
+import { store, persistor } from "./store";
 import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
-import { CssBaseline, ThemeProvider, createTheme} from "@mui/material";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { PersistGate } from "redux-persist/integration/react";
+
 
 const theme = createTheme({
   palette: {
@@ -17,10 +19,12 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HashRouter>
       <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <App />
-        </ThemeProvider>
+        <PersistGate loading={null} persistor={persistor}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+          </ThemeProvider>
+        </PersistGate>
       </Provider>
     </HashRouter>
   </StrictMode>
